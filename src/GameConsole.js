e0dc59b24477c2077defbc99767bc354426af026
mjmj0217 <mjmj0217@naver.com>
2023-07-26 22:41:05 +0900

    feat : 입출력 담당 class

---
+++ src/GameConsole.js
@@ -0,0 +1,50 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+class GameConsole {
+  #outputs = {
+    START_MESSAGE: "숫자 야구 게임을 시작합니다.",
+    REQUIRE_NUMBERS: "숫자를 입력해주세요 : ",
+    REQUIRE_CMD: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    END_MESSAGE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  };
+
+  printStart() {
+    Console.print(this.#outputs.START_MESSAGE);
+  }
+  printResult(strike, ball) {
+    if (strike === 0 && ball === 0) {
+      Console.print("낫싱");
+      return;
+    }
+    if (strike === 0) {
+      Console.print(ball + "볼");
+      return;
+    }
+    if (ball === 0) {
+      Console.print(strike + "스트라이크");
+      return;
+    }
+    Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
+
+  printEnd() {
+    Console.print(this.#outputs.END_MESSAGE);
+  }
+
+  inputNumbers(callback) {
+    Console.readLine(this.#outputs.REQUIRE_NUMBERS, (numbers) => {
+      //validate input
+      return callback(numbers);
+    });
+  }
+  inputCmd(callback) {
+    Console.readLine(this.#outputs.REQUIRE_CMD, (cmd) => {
+      //validate input
+      return callback(cmd);
+    });
+  }
+}
+
+module.exports = {
+  GameConsole,
+};
