6d4d12d87aec8f9f28eaa9d4ddb58066662e77e1
Yeonju Jo <yjj0287@gmail.com>
2023-07-27 19:54:41 +0900

    feat: baseball model 정의

---
+++ src/model/BaseballGame.js
@@ -0,0 +1,24 @@
+class BaseballGame {
+  #answer;
+
+  constructor(answer) {
+    this.#answer = answer;
+  }
+
+  getStrikesAndBalls(userGuess) {
+    let strikes = 0;
+    let balls = 0;
+
+    for (let i = 0; i < this.#answer.length; i++) {
+      if (this.#answer[i] === userGuess[i]) {
+        strikes++;
+      } else if (this.#answer.includes(userGuess[i])) {
+        balls++;
+      }
+    }
+
+    return { strikes, balls };
+  }
+}
+
+module.exports = BaseballGame;
