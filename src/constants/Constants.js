d7afa2ce050ce858c266b129e79ee2b0f794e39f
Yeonju Jo <yjj0287@gmail.com>
2023-07-27 19:38:12 +0900

    feat: 상수값 추가

---
+++ src/constants/Constants.js
@@ -0,0 +1,13 @@
+const MESSAGE_MC = Object.freeze({
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+});
+
+const MESSAGE_ERROR = Object.freeze({
+  INPUT: "입력값이 유효하지 않습니다. 서로 다른 숫자 3자리를 입력해주세요.",
+  RESTART: "입력값이 유효하지 않습니다. 1, 2 중에 한 개만 입력주세요.",
+});
+
+module.exports = { MESSAGE_MC, MESSAGE_ERROR };
