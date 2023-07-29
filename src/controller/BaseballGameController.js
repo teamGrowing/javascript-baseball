09cd8404329f3bb77e7d00ed1a8550776c0787bd
Yeonju Jo <yjj0287@gmail.com>
2023-07-29 19:06:07 +0900

    feat: App에 게임 시작 로직 추가

--- src/controller/BaseballGameController.js
+++ src/controller/BaseballGameController.js
@@ -1,11 +1,18 @@
 const BaseballGame = require("../model/BaseballGame");
+const BaseballGameView = require("../view/BaseballGameView");
 const generateRandomNumber = require("../utils/RandomNumberGenerator");
 
 class BaseballGameController {
   #baseballGame;
+  #baseballGameView;
 
   constructor() {
     this.#baseballGame = new BaseballGame(generateRandomNumber());
+    this.#baseballGameView = new BaseballGameView();
+  }
+
+  startGame() {
+    this.#baseballGameView.displayWelcomeMessage();
   }
 }
 
